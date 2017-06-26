73c5494e979e069b680c8a85c8fc7d2165143e73
Huu Vien Le <vien.lh@neo-lab.vn>
2017-06-26 17:13:53 +0700

    Create view Event,Contact, Project ...etc

---
+++ resources/views/admin/event/edit.blade.php
@@ -0,0 +1,134 @@
+@extends('admin.master')
+@section('page-title') Edit Event @endsection
+@section('css')
+  <!-- Bootstrap time Picker -->
+  <link rel="stylesheet" href="{{ asset('admin') }}/plugins/timepicker/bootstrap-timepicker.min.css">
+  <link rel="stylesheet" href="{{ asset('admin') }}/dist/css/skins/_all-skins.min.css">
+  <style type="text/css">
+    .dropdown-menu{
+      left: 25%!important;
+    }
+  </style>
+@endsection
+@section('content-wrapper')
+<!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Add new Event
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Event</a></li>
+        <li class="active">Add</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">     
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">            
+            <div class="box-header with-border">
+              @include('admin.include.message')
+              <h3 class="box-title"></h3>
+            </div>
+            
+            <form class="form-horizontal" method="POST" action="" enctype="multipart/form-data">
+
+              <div class="box-body">
+                  <div class="form-group">
+                    <label for="title" class="col-sm-3 control-label">Event Title</label>
+
+                    <div class="input-group col-sm-5">
+                      <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" placeholder="Title">
+                    </div>
+                    <!-- /.input group -->
+                  </div>
+                  <!-- /.form group -->
+              </div>
+
+              <div class="box-body">
+                  <div class="form-group">
+                    <label for="content" class="col-sm-3 control-label">Description</label>
+
+                    <div class="input-group col-sm-5">
+                      <input type="text" class="form-control" name="content" id="content" value="{{ old('content') }}" placeholder="Description">
+                    </div>
+                    <!-- /.input group -->
+                  </div>
+                  <!-- /.form group -->
+              </div>
+
+              <div class="box-body">
+                  <div class="form-group">
+                    <label for="date-begin" class="col-sm-3 control-label">Date Begin</label>
+
+                    <div class="input-group col-sm-5">
+                      <input type="text" class="form-control" name="date-begin" id="date-begin" value="{{ old('date-begin') }}" placeholder="Date Begin">
+                    </div>
+                    <!-- /.input group -->
+                  </div>
+                  <!-- /.form group -->
+              </div>
+
+              <div class="box-body">
+                <!-- time Picker -->
+                <div class="bootstrap-timepicker">
+                  <div class="form-group">
+                    <label for="time-begin" class="col-sm-3 control-label">Time begin</label>
+
+                    <div class="input-group col-md-5">
+                      <input type="text" class="form-control timepicker">
+                    </div>
+                    <!-- /.input group -->
+                  </div>
+                  <!-- /.form group -->
+                </div>
+              </div>
+              <div class="box-footer">
+                <div class="col-sm-3">                  
+                  <a href="#" class="btn btn-default pull-right">Return List </a>
+                </div>
+                <div class="col-sm-5">
+                  <button type="submit" class="btn btn-info pull-right">Add New</button>
+                </div>
+              </div>
+            </form>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+        </div>
+        <!-- /.col (left) -->
+        <div class="col-md-6">
+          <!-- /.box -->
+
+          <!-- /.box -->
+        </div>
+        <!-- /.col (right) -->
+      </div>
+      <!-- /.row -->
+
+    </section>
+    <!-- /.content -->
+@endsection
+@section('script')
+  <!-- InputMask -->
+  <script src="{{ asset('admin') }}/plugins/input-mask/jquery.inputmask.js"></script>
+  <script src="{{ asset('admin') }}/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+  <script src="{{ asset('admin') }}/plugins/input-mask/jquery.inputmask.extensions.js"></script>
+  <!-- bootstrap time picker -->
+  <script src="{{ asset('admin') }}/plugins/timepicker/bootstrap-timepicker.min.js"></script>
+  <script>
+    $(function () {      
+      //date picker
+      $('#datepicker').datepicker({
+          autoclose: true
+        });
+      //Timepicker
+      $(".timepicker").timepicker({
+        showInputs: false
+      });
+    });
+  </script>
+@endsection
